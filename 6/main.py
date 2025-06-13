ab6af5399874a32bf143dfd6fce0b8c385a3d6b1
Santosh Bhandari <santoshvandari1@gmail.com>
2025-06-13 15:11:53 +0545

    feat: Question 5

---
+++ 6/main.py
@@ -0,0 +1,12 @@
+class Solution:
+    # Function to find values in array equal to their indices
+    def valueEqualToIndex(self, arr):
+        ans = []  # create an empty list to store the values
+        n = len(arr)
+        # iterate through the array
+        for i in range(n):
+            # check if the value at index i is equal to its index + 1
+            if arr[i] == i + 1:
+                ans.append(arr[i])  # if true, add the value to the list
+        return ans  # return the list of values that are equal to their indices
+
